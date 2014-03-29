0cff56beac66080d1e5eccd23d2ca80edd444a19
Osamu Ishikawa <bass.duo@gmail.com>
2014-03-29 19:57:00 +0900

    generate maven project

---
+++ study-1/src/main/java/org/ajm-web/App.java
@@ -0,0 +1,13 @@
+package org.ajm-web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
