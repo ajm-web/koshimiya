c85eca7f9cd912193d31186bd256e05d4cd5d462
Osamu Ishikawa <bass.duo@gmail.com>
2014-03-29 23:36:10 +0900

    use java8

--- study-2/src/main/java/org/ajm_web/study2/PasscheckImpl.java
+++ study-2/src/main/java/org/ajm_web/study2/PasscheckImpl.java
@@ -1,32 +1,17 @@
 package org.ajm_web.study2;
 
-/**
- * Created by jami on 2014/03/29.
- */
+import java.util.Optional;
+
 public class PasscheckImpl implements Passcheck{
     @Override
     public boolean check(String str) {
 
-        //文字数チェック
-        if(str.length() < 10){
-            return false;
-        }
+        return Optional.ofNullable(str)
+                .filter(s -> s.length() >= 10)
+                .filter(s -> s.matches(".*[0-9].*"))
+                .filter(s -> s.matches(".*[A-Z].*"))
+                .filter(s -> s.matches(".*[a-z].*"))
+                .isPresent();
 
-        //数字チェック
-        if(!str.matches(".*[0-9].*")){
-            return false;
-        }
-
-        //小文字アルファベットチェック
-        if(!str.matches(".*[a-z].*")){
-            return false;
-        }
-
-        //大文字アルファベットチェック
-        if(!str.matches(".*[A-Z].*")){
-            return false;
-        }
-
-        return true;
     }
 }
